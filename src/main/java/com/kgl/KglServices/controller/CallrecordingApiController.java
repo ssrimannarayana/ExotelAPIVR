47f534d9977910e2db534f94240191a576094b7f
venkeykfl <venkatesh.jonnakuti@gmail.com>
2024-02-24 11:42:00 +0530

    new source added

---
+++ src/main/java/com/kgl/KglServices/controller/CallrecordingApiController.java
@@ -0,0 +1,143 @@
+package com.kgl.KglServices.controller;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kgl.KglServices.model.CampaignPojo;
+import com.kgl.KglServices.services.PhonePeServcies;
+
+@RestController
+@RequestMapping({ "/call" })
+public class CallrecordingApiController {
+
+	private static final Logger logger = LoggerFactory.getLogger(CallrecordingApiController.class);
+
+	@Autowired
+	private PhonePeServcies phpeServices;
+
+	@Value("${AP_CAMPAIGN_JSON_RESPONSE_URL}")
+	private String AP_googlesheet_data_restapi;
+
+	@Value("${END_TIME}")
+	private String end_time;
+
+	@GetMapping("/testApi")
+	public String test() {
+		//sendMail();
+		return "hi";
+	}
+
+	@GetMapping("/ApIvrCampaign")
+	public Boolean ExotelCampaignAPI() throws Exception {
+		boolean ivrStatus = false;
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		Date startTime = new SimpleDateFormat("HH:mm:ss").parse(dtf.format(now));
+		Date endTime = new SimpleDateFormat("HH:mm:ss").parse(end_time);
+		if (startTime.before(endTime)) {
+			String AP_URL = AP_googlesheet_data_restapi;
+			List<CampaignPojo> list = getCampData(AP_URL);
+			int recordsSize = list.size();
+			logger.info("AP IVR google sheet api records count:::" + recordsSize);
+			if (recordsSize > 0) {
+				return phpeServices.startIvrCampaignLists(list);
+			}
+		} else {
+			logger.info("AP IVR service stopped due to time out");
+			return false;
+		}
+		sendMail();
+		return ivrStatus;
+	}
+
+	private void sendMail() {
+		// TODO Auto-generated method stub
+		String to = "operations.it@kanakadurgafinance.com";// change accordingly
+		// Sender's email ID needs to be mentioned
+		String from = "venkat@kanakadurgafinance.com";// change accordingly
+		final String username = "venkat@kanakadurgafinance.com";// change accordingly
+		final String password = "rfxhzjywnqgcggpm";// change accordingly
+		// Assuming you are sending email through relay.jangosmtp.net
+		String host = "smtp.gmail.com";
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "587");
+
+		// Get the Session object.
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+			// Create a default MimeMessage object.
+			Message message = new MimeMessage(session);
+
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+
+			// Set To: header field of the header.
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+			// Set Subject: header field
+			message.setSubject("APIVR CAMPAINGS ENDED");
+
+			// Now set the actual message
+			message.setText("Hi, Bargavi APIVR Call Campaigns Completed. Plz Tigger Callback API");
+
+			// Send message
+			Transport.send(message);
+
+			System.out.println("Sent message successfully....");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private List<CampaignPojo> getCampData(String campaign_ListUrls)
+			throws JsonMappingException, JsonProcessingException {
+		List<CampaignPojo> responseDataObj = null;
+		try {
+			ResponseEntity<String> response = new RestTemplate().getForEntity(campaign_ListUrls, String.class);
+			ObjectMapper objectMapper;
+			if (response != null) {
+				String stringArry = response.getBody().toString();
+				objectMapper = new ObjectMapper();
+				responseDataObj = objectMapper.readValue(stringArry, new TypeReference<List<CampaignPojo>>() {
+				});
+			}
+		} catch (Exception e) {
+			logger.info("google sheet api response No records found:::" + e);
+		}
+		return responseDataObj;
+	}
+
+	@GetMapping("/ApGoogleSheetUpdate")
+	public Boolean updateCampaignAPI() throws Exception {
+		return phpeServices.updateCampaignDataIntoGoogleSheet();
+	}
+
+}
